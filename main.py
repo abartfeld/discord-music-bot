8dad31c4d27d077ede2180a393ac9ff5f3367f32
Andrew Bartfeld <bartfeldandrew@gmail.com>
2019-01-27 18:07:17 -0800

    Changed instances of time.sleep to asyncio.sleep to prevent blocking

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import discord
 from discord.ext import commands
 import random
+import asyncio
 import time
 
 description = "images & words bot!"
 bot = commands.Bot(command_prefix='$', description=description)
 LP_CHANNEL_IDS = [419732859360641024, 457320312124604416]
-TEST_CHANNEL_IDS = [419732859360641024, 457320312124604416, 430646748487221261, 455253321067003927]
+TEST_CHANNEL_IDS = [539174471504756757, 419732859360641024, 457320312124604416, 430646748487221261, 455253321067003927]
 
 #CD_ALLOWED = {x: True for x in LP_CHANNEL_IDS}
 CD_ALLOWED = {x: True for x in TEST_CHANNEL_IDS}
@@ -21,18 +22,20 @@ async def on_ready():
 @bot.command()
 async def cd(ctx):
     """Auto countdown from 10 for LPs. Only allowed in LP channels."""
-    if ctx.channel.id in CD_ALLOWED: 
+    if ctx.channel.id in CD_ALLOWED:
         if CD_ALLOWED[ctx.channel.id]:
             CD_ALLOWED[ctx.channel.id] = False
             await ctx.send('Counting down!')
-            time.sleep(2)
+            await asyncio.sleep(2)
             for i in range(10,0, -1):
                 await ctx.send(i)
-                time.sleep(1)
+                await asyncio.sleep(1)
             await ctx.send('GO')
             CD_ALLOWED[ctx.channel.id] = True
     else:
         await ctx.send('Countdown is allowed only in LP channels.')
+
+
 
 # Make sure you have a token.txt in the same directory as main.py containing ONLY the bot token
 token = open('token.txt', 'r').read()
