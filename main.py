1e4fa8cb4958641a065d475d3c35d2165e61c2fd
Andrew Bartfeld <bartfeldandrew@gmail.com>
2019-01-27 17:42:25 -0800

    Changed allowed cd channels for testing purposes

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ import time
 description = "images & words bot!"
 bot = commands.Bot(command_prefix='$', description=description)
 LP_CHANNEL_IDS = [419732859360641024, 457320312124604416]
-#TEST_CHANNEL_IDS = [419732859360641024, 457320312124604416, 430646748487221261, 455253321067003927]
+TEST_CHANNEL_IDS = [419732859360641024, 457320312124604416, 430646748487221261, 455253321067003927]
 
-CD_ALLOWED = {x: True for x in LP_CHANNEL_IDS}
-#CD_ALLOWED = {x: True for x in TEST_CHANNEL_IDS}
+#CD_ALLOWED = {x: True for x in LP_CHANNEL_IDS}
+CD_ALLOWED = {x: True for x in TEST_CHANNEL_IDS}
 
 @bot.event
 async def on_ready():
@@ -24,6 +24,8 @@ async def cd(ctx):
     if ctx.channel.id in CD_ALLOWED: 
         if CD_ALLOWED[ctx.channel.id]:
             CD_ALLOWED[ctx.channel.id] = False
+            await ctx.send('Counting down!')
+            time.sleep(2)
             for i in range(10,0, -1):
                 await ctx.send(i)
                 time.sleep(1)
