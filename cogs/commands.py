a7b232aec649f74d27e61b96695e52a16b5234dc
Andrew Bartfeld <acbartfe@calpoly.edu>
2019-02-02 16:23:49 -0800

    Restructured code

---
+++ cogs/commands.py
@@ -0,0 +1,38 @@
+import discord
+import asyncio
+import time
+from discord.ext import commands
+
+# LP_CHANNEL_IDS = [419732859360641024, 457320312124604416]
+TEST_CHANNEL_IDS = [539174471504756757]
+
+CD_ALLOWED = {x: True for x in TEST_CHANNEL_IDS}
+
+class Commands:
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def cd(self, ctx):
+        """Auto countdown from 10 for LPs. Only allowed in LP channels."""
+        if ctx.channel.id in CD_ALLOWED:
+            if CD_ALLOWED[ctx.channel.id]:
+                CD_ALLOWED[ctx.channel.id] = False
+                await ctx.send('Counting down!')
+                await asyncio.sleep(2)
+                for i in range(10, 0, -1):
+                    await ctx.send(i)
+                    await asyncio.sleep(1)
+                await ctx.send('GO')
+                CD_ALLOWED[ctx.channel.id] = True
+        else:
+            await ctx.send('Countdown is allowed only in LP channels.')
+
+    @commands.command()
+    async def close(self, ctx):
+        if 'Mod' in [x.name for x in ctx.author.roles] or 'Dev' in [x.name for x in ctx.author.roles]:
+            await ctx.bot.close()
+
+def setup(bot):
+    bot.add_cog(Commands(bot))
---
+++ prog_bot.py
@@ -0,0 +1,45 @@
+import discord
+import asyncio
+import random
+import sys
+
+from discord.ext import commands
+
+token = open('token.txt', 'r').read()
+
+EXTENSIONS = ['cogs.commands']
+
+class ProgBot(commands.Bot):
+
+    def __init__(self):
+        super().__init__(command_prefix='$', description="Images & Words Bot")
+        self.token = token
+
+        for ext in EXTENSIONS:
+            try:
+                self.load_extension(ext)
+            except Exception as e:
+                print(f'Failed to load extension {ext}', file=sys.stderr)
+
+    async def on_ready(self):
+        print('Logged in as')
+        print(self.user.name)
+        print(self.user.id)
+        print('------')
+
+    async def on_message(self, message):
+        if message.author.bot:
+            return
+        await self.process_commands(message)
+
+    # async def on_reaction(self, react):
+
+    async def close(self):
+        await super().close()
+
+    def run(self):
+        super().run(self.token, reconnect=True)
+
+if __name__ == '__main__':
+    prog_bot = ProgBot()
+    prog_bot.run()
