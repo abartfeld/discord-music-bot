42a8c7b50acd3dfabcd59df3ff5f219166ecfd5d
Andrew Bartfeld <bartfeldandrew@gmail.com>
2019-02-08 15:34:53 -0800

    Implementing change requests from pull and fixing minor typing issue

--- cogs/commands.py
+++ cogs/commands.py
@@ -6,9 +6,6 @@ from discord.ext import commands
 LP_CHANNEL_IDS = [419732859360641024, 457320312124604416]
 TEST_CHANNEL_IDS = [541472338013847553, 533773210345275401]
 
-CD_ALLOWED = {x: True for x in LP_CHANNEL_IDS}
-
-
 class Commands:
 
     def __init__(self, bot):
@@ -17,47 +14,42 @@ class Commands:
     @commands.command()
     async def cd(self, ctx):
         """Auto countdown from 10 for LPs. Only allowed in LP channels."""
-        if ctx.channel.id in CD_ALLOWED:
-            if CD_ALLOWED[ctx.channel.id]:
-                CD_ALLOWED[ctx.channel.id] = False
-                await ctx.send('Counting down!')
-                await asyncio.sleep(2)
-                for i in range(10, 0, -1):
-                    await ctx.send(i)
-                    await asyncio.sleep(1)
-                await ctx.send('GO')
-                CD_ALLOWED[ctx.channel.id] = True
+        if ctx.channel.id in LP_CHANNEL_IDS:
+            await ctx.send('Counting down!')
+            await asyncio.sleep(2)
+            for i in range(10, 0, -1):
+                await ctx.send(i)
+                await asyncio.sleep(1)
+            await ctx.send('GO')
         else:
             await ctx.send('Countdown is allowed only in LP channels!')
 
     @commands.command()
     async def timestamp(self, ctx):
-        if ctx.channel.id in CD_ALLOWED:
+        if ctx.channel.id in LP_CHANNEL_IDS:
             spotify = None
-            if ctx.author.activities and ctx.author.activities[0] == discord.Spotify:
+            if ctx.author.activities and any(isinstance(a, discord.Spotify) for a in ctx.author.activities):
                 spotify = ctx.author.activities[0]
-            while spotify == None:
+            if spotify == None:
                 async for message in ctx.channel.history(limit=50):
                     act = message.author.activities
-                    if act and type(act[0]) == discord.Spotify:
+                    if act and any(isinstance(a, discord.Spotify) for a in act):
                         spotify = act[0]
                     else:
                         continue
-                if spotify == None:
-                    await ctx.send("Can't find timestamp info!")
-                    return
+            if spotify == None:
+                await ctx.send("Can't find timestamp info!")
+                return
 
             seconds = (datetime.datetime.utcnow() - spotify.start + datetime.timedelta(seconds=3)).total_seconds()
             minutes = (seconds % 3600) // 60
             seconds = seconds % 60
-            if seconds < 10:
-                seconds = str(0) + str(int(seconds))
-            else:
-                seconds = str(int(seconds))
+            seconds = str(int(seconds)).zfill(2)
 
             song = discord.Embed(type='rich', title="Listening Party:", color=2351402)
             song.set_thumbnail(url=spotify.album_cover_url)
-            song.add_field(value="[{} - {}](https://open.spotify.com/track/{})".format(spotify.title, spotify.artist, spotify.track_id),
+            song.add_field(value="[{} - {}](https://open.spotify.com/track/{})".format(spotify.artist, spotify.title,
+                                                                                       spotify.track_id),
                            name="Currently at {}:{}".format(int(minutes), seconds))
 
             await ctx.send(embed=song)
